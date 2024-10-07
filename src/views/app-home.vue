ceef1fefaaad704373a5bcc6bf0dc088d9289709
Par9uet <1607611087@qq.com>
2024-10-07 16:51:54 +0800

    feat: 更美观的首页骨架屏

--- src/views/app-home.vue
+++ src/views/app-home.vue
@@ -28,27 +28,51 @@ const slidesPerView = computed(() => {
 </script>
 
 <template>
-  <a-spin :spinning="loading">
-    <a-row :gutter="[16, 16]">
-      <a-col v-for="item of data.data" :key="item.id" :span="24">
-        <a-card :title="item.title">
-          <swiper
-            class="select-none"
-            :slides-per-view="slidesPerView"
-            :space-between="16"
-            :modules="[Pagination]"
-            :pagination="{
-              clickable: true,
+  <a-row v-if="loading" :gutter="[16, 16]">
+    <a-col v-for="item of 4" :key="item" :span="24">
+      <a-card>
+        <template #title>
+          <a-skeleton
+            class="title-skeleton"
+            :paragraph="false"
+            :title="{
+              width: '30%',
             }"
-          >
-            <swiper-slide v-for="subItem of item.list" :key="subItem.id">
-              <comic-item :comic="subItem" />
-            </swiper-slide>
-          </swiper>
-        </a-card>
-      </a-col>
-    </a-row>
-  </a-spin>
+          />
+        </template>
+        <a-row :gutter="[16, 16]">
+          <a-col v-for="subItem of 4" :key="subItem" :span="6">
+            <comic-item-skeleton />
+          </a-col>
+        </a-row>
+      </a-card>
+    </a-col>
+  </a-row>
+  <a-row v-else :gutter="[16, 16]">
+    <a-col v-for="item of data.data" :key="item.id" :span="24">
+      <a-card :title="item.title">
+        <swiper
+          class="select-none"
+          :slides-per-view="slidesPerView"
+          :space-between="16"
+          :modules="[Pagination]"
+          :pagination="{
+            clickable: true,
+          }"
+        >
+          <swiper-slide v-for="subItem of item.list" :key="subItem.id">
+            <comic-item :comic="subItem" />
+          </swiper-slide>
+        </swiper>
+      </a-card>
+    </a-col>
+  </a-row>
 </template>
 
-<style scoped></style>
+<style scoped lang="less">
+.title-skeleton {
+  :deep(.ant-skeleton-title) {
+    margin-bottom: 0;
+  }
+}
+</style>
