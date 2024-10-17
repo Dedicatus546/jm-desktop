ff6d87ee89190bd361679a904aa71ba8120258c5
Par9uet <1607611087@qq.com>
2024-10-17 17:13:42 +0800

    feat: vue 全局错误捕获钩子写入错误日志

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,19 @@ import dayjs from "dayjs";
 import { createApp } from "vue";
 
 import App from "./App.vue";
+import logger from "./logger";
 import router from "./router";
 import pinia from "./store";
 
 dayjs.locale("zh-cn");
 
 const app = createApp(App);
+
+app.config.errorHandler = (err, _instance, info) => {
+  logger.error(`[vue] 全局捕获错误，错误码 ${info} 错误信息 ${String(err)}`);
+};
+
+app.config.performance = true;
 
 app.use(pinia).use(router);
 
