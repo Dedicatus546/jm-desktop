3bce8e0f364c83ce892888621ce58d67e5624357
Par9uet <1607611087@qq.com>
2024-10-17 13:49:07 +0800

    chore: 修复 lint 命令不正确问题

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,7 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ["**/dist", "**/.eslintrc.cjs"],
+    ignores: ["data/**/*", "dist-electron/**/*", "**/.eslintrc.cjs", ""],
   },
   ...compat.extends(
     "eslint:recommended",
