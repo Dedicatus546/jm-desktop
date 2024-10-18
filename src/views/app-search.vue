5ac67e478cd98d7cfbd1044525681866377d5501
Par9uet <1607611087@qq.com>
2024-10-18 21:01:41 +0800

    refactor: 调整搜索页样式

--- src/views/app-search.vue
+++ src/views/app-search.vue
@@ -50,32 +50,32 @@ const { page, total, data, send, loading } = usePagination(
       </a-input-search>
     </a-col>
     <a-col :span="24">
-      <a-spin :spinning="loading">
-        <a-row v-if="total" :gutter="[16, 16]">
-          <a-col v-for="item of data" :key="item.id" :sm="8" :xl="6" :xxl="4">
-            <comic-item :comic="item" />
-          </a-col>
-          <a-col :span="24">
-            <a-pagination
-              v-model:current="page"
-              align="right"
-              :page-size="80"
-              :total="total"
-              show-quick-jumper
-              :show-size-changer="false"
-            />
-          </a-col>
-        </a-row>
-        <a-col v-else :span="24">
-          <a-card>
+      <a-card>
+        <a-spin :spinning="loading">
+          <a-row v-if="total" :gutter="[16, 16]">
+            <a-col v-for="item of data" :key="item.id" :sm="8" :xl="6" :xxl="4">
+              <comic-item :comic="item" />
+            </a-col>
+            <a-col :span="24">
+              <a-pagination
+                v-model:current="page"
+                align="right"
+                :page-size="80"
+                :total="total"
+                show-quick-jumper
+                :show-size-changer="false"
+              />
+            </a-col>
+          </a-row>
+          <a-col v-else :span="24">
             <a-empty>
               <a-typography-text type="secondary">
                 使用上面的搜索框来搜索作品
               </a-typography-text>
             </a-empty>
-          </a-card>
-        </a-col>
-      </a-spin>
+          </a-col>
+        </a-spin>
+      </a-card>
     </a-col>
   </a-row>
 </template>
