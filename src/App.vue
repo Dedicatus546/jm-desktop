d4fae32adabed9b07fdceeb5c1536d9e2a125b66
Par9uet <1607611087@qq.com>
2024-11-09 18:50:02 +0800

    refactor: 应用初始化显示基本的头部，可在初始化失败时进入设置页设置

--- src/App.vue
+++ src/App.vue
@@ -14,40 +14,37 @@ useRecoveryScrollPosition(scrollViewRef);
     <v-app>
       <app-dialog-provider>
         <app-snackbar-provider location="top right">
-          <div v-if="error"></div>
-          <div
-            v-if="loading || error"
-            class="w-full h-full flex items-center justify-center"
-          >
-            <div v-if="loading" class="flex flex-col items-center gap-4">
-              <v-progress-circular indeterminate></v-progress-circular>
-              <span>{{ currentStatus }}</span>
-            </div>
-            <div v-if="error" class="flex flex-col items-center gap-4">
-              {{ error }}
-              <v-btn type="primary" @click="reInit()">重新加载</v-btn>
-            </div>
-          </div>
-          <template v-else>
-            <app-header />
-            <v-main>
-              <v-container
-                id="scroll-view"
-                ref="scrollViewRef"
-                fluid
-                style="height: calc(100vh - var(--v-layout-top, 0px))"
-                class="w-full h-full overflow-y-auto relative"
+          <app-header :simple="!!(loading || error)" />
+          <v-main>
+            <v-container
+              id="scroll-view"
+              ref="scrollViewRef"
+              fluid
+              style="height: calc(100vh - var(--v-layout-top, 0px))"
+              class="w-full h-full overflow-y-auto relative"
+            >
+              <div
+                v-if="loading || error"
+                class="w-full h-full flex items-center justify-center"
               >
-                <router-view v-slot="{ Component }">
-                  <keep-alive
-                    include="app-home,app-search,app-person,app-category"
-                  >
-                    <component :is="Component" />
-                  </keep-alive>
-                </router-view>
-              </v-container>
-            </v-main>
-          </template>
+                <div v-if="loading" class="flex flex-col items-center gap-4">
+                  <v-progress-circular indeterminate></v-progress-circular>
+                  <span>{{ currentStatus }}</span>
+                </div>
+                <div v-if="error" class="flex flex-col items-center gap-4">
+                  {{ error }}
+                  <v-btn type="primary" @click="reInit()">重新加载</v-btn>
+                </div>
+              </div>
+              <router-view v-else v-slot="{ Component }">
+                <keep-alive
+                  include="app-home,app-search,app-person,app-category"
+                >
+                  <component :is="Component" />
+                </keep-alive>
+              </router-view>
+            </v-container>
+          </v-main>
         </app-snackbar-provider>
       </app-dialog-provider>
     </v-app>
--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -11,6 +11,10 @@ import useIpcRendererInvoke from "@/compositions/use-ipc-renderer-invoke";
 import useAppStore from "@/stores/use-app-store";
 import useUserStore from "@/stores/use-user-store";
 
+defineProps<{
+  simple: boolean;
+}>();
+
 const appStore = useAppStore();
 const userStore = useUserStore();
 const userInfo = computed(() => userStore.userInfo);
@@ -70,108 +74,110 @@ const logout = () => {
     </v-app-bar-title>
     <template #append>
       <div class="app-region-nodrag">
-        <app-header-icon-btn
-          tooltip-text="返回"
-          icon="mdi-arrow-u-left-top"
-          @click="router.back()"
-        />
-        <app-header-icon-btn
-          :tooltip-text="`切换${appStore.config.mode === 'dark' ? '日间模式' : '夜间模式'}`"
-          icon="mdi-swap-horizontal"
-          @click="
-            changeMode(appStore.config.mode === 'dark' ? 'light' : 'dark')
-          "
-        />
-        <app-header-icon-btn
-          tooltip-text="每月签到"
-          icon="mdi-calendar-month"
-          @click="
-            router.push({
-              name: 'SIGN_IN',
-            })
-          "
-        />
-        <app-header-icon-btn
-          tooltip-text="本子搜索"
-          icon="mdi-magnify"
-          @click="
-            router.push({
-              name: 'SEARCH',
-            })
-          "
-        />
-        <app-header-icon-btn
-          tooltip-text="本子分类"
-          icon="mdi-tag"
-          @click="
-            router.push({
-              name: 'CATEGORY',
-            })
-          "
-        />
-        <app-header-icon-btn
-          tooltip-text="每周必看"
-          icon="mdi-eye"
-          @click="
-            router.push({
-              name: 'WEEK',
-            })
-          "
-        />
-        <v-menu>
-          <template #activator="{ props }">
-            <v-btn v-bind="props" icon="mdi-link"></v-btn>
-          </template>
-          <v-list>
-            <v-list-item @click="openLinkIpc(appStore.setting.webHost)">
-              <v-list-item-title>官方站点</v-list-item-title>
-            </v-list-item>
-            <v-list-item @click="openLinkIpc(appStore.setting.storeLink.web)">
-              <v-list-item-title>下载页面</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-        <template v-if="userInfo">
+        <template v-if="!simple">
           <app-header-icon-btn
-            tooltip-text="个人中心"
-            icon="mdi-account"
+            tooltip-text="返回"
+            icon="mdi-arrow-u-left-top"
+            @click="router.back()"
+          />
+          <app-header-icon-btn
+            :tooltip-text="`切换${appStore.config.mode === 'dark' ? '日间模式' : '夜间模式'}`"
+            icon="mdi-swap-horizontal"
+            @click="
+              changeMode(appStore.config.mode === 'dark' ? 'light' : 'dark')
+            "
+          />
+          <app-header-icon-btn
+            tooltip-text="每月签到"
+            icon="mdi-calendar-month"
             @click="
               router.push({
-                name: 'PERSON',
+                name: 'SIGN_IN',
               })
             "
           />
           <app-header-icon-btn
-            tooltip-text="退出"
-            icon="mdi-logout"
-            @click="logout"
+            tooltip-text="本子搜索"
+            icon="mdi-magnify"
+            @click="
+              router.push({
+                name: 'SEARCH',
+              })
+            "
+          />
+          <app-header-icon-btn
+            tooltip-text="本子分类"
+            icon="mdi-tag"
+            @click="
+              router.push({
+                name: 'CATEGORY',
+              })
+            "
+          />
+          <app-header-icon-btn
+            tooltip-text="每周必看"
+            icon="mdi-eye"
+            @click="
+              router.push({
+                name: 'WEEK',
+              })
+            "
+          />
+          <v-menu>
+            <template #activator="{ props }">
+              <v-btn v-bind="props" icon="mdi-link"></v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="openLinkIpc(appStore.setting.webHost)">
+                <v-list-item-title>官方站点</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="openLinkIpc(appStore.setting.storeLink.web)">
+                <v-list-item-title>下载页面</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+          <template v-if="userInfo">
+            <app-header-icon-btn
+              tooltip-text="个人中心"
+              icon="mdi-account"
+              @click="
+                router.push({
+                  name: 'PERSON',
+                })
+              "
+            />
+            <app-header-icon-btn
+              tooltip-text="退出"
+              icon="mdi-logout"
+              @click="logout"
+            />
+          </template>
+          <app-header-icon-btn
+            v-else
+            tooltip-text="登录"
+            icon="mdi-login"
+            @click="
+              router.push({
+                name: 'LOGIN',
+              })
+            "
+          />
+          <app-header-icon-btn
+            tooltip-text="下载"
+            icon="mdi-download"
+            @click="
+              router.push({
+                name: 'DOWNLOAD',
+              })
+            "
           />
         </template>
-        <app-header-icon-btn
-          v-else
-          tooltip-text="登录"
-          icon="mdi-login"
-          @click="
-            router.push({
-              name: 'LOGIN',
-            })
-          "
-        />
         <app-header-icon-btn
           tooltip-text="设置"
           icon="mdi-cog"
           @click="
             router.push({
               name: 'CONFIG',
-            })
-          "
-        />
-        <app-header-icon-btn
-          tooltip-text="下载"
-          icon="mdi-download"
-          @click="
-            router.push({
-              name: 'DOWNLOAD',
             })
           "
         />
