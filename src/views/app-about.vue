b5059dd830d62ee9489977b16163277df61a7d96
Par9uet <1607611087@qq.com>
2024-11-15 12:42:44 +0800

    refactor: 关于页显示 commit hash 和 图标

--- src/views/app-about.vue
+++ src/views/app-about.vue
@@ -1,21 +1,38 @@
-<script setup lang="tsx">
+<script setup lang="ts">
+import { openLinkIpc } from "@/apis";
 import { version } from "@@/package.json";
+const hash = __COMIT_HASH__;
+const isDev = import.meta.env.DEV;
+
+const toRepo = () => {
+  openLinkIpc("https://github.com/Dedicatus546/jm-desktop");
+};
 </script>
 
 <template>
   <v-card title="关于">
     <v-card-text>
+      <div class="flex flex-col gap-4 items-center py-4">
+        <v-avatar size="100" image="/png/512x512.png" rounded="0" />
+        <div class="text-h5">jm-desktop</div>
+      </div>
+      <v-divider />
       <v-list>
         <v-list-item title="版本">
           <template #append>
             {{ version }}
           </template>
         </v-list-item>
+        <v-list-item title="commit">
+          <template #append>
+            {{ isDev ? "DEV" : hash }}
+          </template>
+        </v-list-item>
         <v-list-item title="仓库地址">
           <template #append>
-            <a href="https://github.com/Dedicatus546/jm-desktop">
+            <div class="cursor-pointer" @click="toRepo">
               https://github.com/Dedicatus546/jm-desktop
-            </a>
+            </div>
           </template>
         </v-list-item>
       </v-list>
