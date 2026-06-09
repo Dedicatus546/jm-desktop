fb97c598d0133f0748403153f0c054c27e58e0ea
Dedicatus546 <qq1607611087@gmail.com>
2026-06-09 10:54:53 +0800

    fix: 修复切换主题窗口没有同步变更问题

--- src/modules/about/main.ts
+++ src/modules/about/main.ts
@@ -1,49 +1,15 @@
-import '../common.ts'
+import '../common-style.ts'
 
 import { createApp } from 'vue'
-import { createVuetify } from 'vuetify'
-import { Intersect } from 'vuetify/directives'
-import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 import App from './App.vue'
 import { error } from '@/utils/logger.ts'
 import { normalizeError } from '@/utils'
 import { registerGlobalState } from '@/utils/resigter-global-state.ts'
+import { createVuetify } from '../create-vuetify.ts'
 
 await registerGlobalState()
 
-const vuetify = createVuetify({
-  theme: {
-    defaultTheme: 'light',
-    themes: {
-      light: {
-        dark: false,
-        colors: {
-          primary: '#ff9800',
-          'on-primary': '#ffffff',
-        },
-      },
-      dark: {
-        dark: true,
-        colors: {
-          primary: '#633b00',
-          'on-primary': '#ffffff',
-        },
-      },
-    },
-  },
-  directives: {
-    Intersect,
-  },
-  icons: {
-    defaultSet: 'mdi',
-    aliases: {
-      ...aliases,
-    },
-    sets: {
-      mdi,
-    },
-  },
-})
+const vuetify = createVuetify()
 
 const app = createApp(App)
 
--- src/modules/home/main.ts
+++ src/modules/home/main.ts
@@ -1,50 +1,16 @@
-import '../common.ts'
+import '../common-style.ts'
 
 import { createApp } from 'vue'
-import { createVuetify } from 'vuetify'
-import { Intersect } from 'vuetify/directives'
-import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 import { error } from '@/utils/logger.ts'
 import { registerGlobalState } from '@/utils/resigter-global-state.ts'
 import { normalizeError } from '@/utils'
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from '../create-vuetify.ts'
 
 await registerGlobalState()
 
-const vuetify = createVuetify({
-  theme: {
-    defaultTheme: 'light',
-    themes: {
-      light: {
-        dark: false,
-        colors: {
-          primary: '#ff9800',
-          'on-primary': '#ffffff',
-        },
-      },
-      dark: {
-        dark: true,
-        colors: {
-          primary: '#633b00',
-          'on-primary': '#ffffff',
-        },
-      },
-    },
-  },
-  directives: {
-    Intersect,
-  },
-  icons: {
-    defaultSet: 'mdi',
-    aliases: {
-      ...aliases,
-    },
-    sets: {
-      mdi,
-    },
-  },
-})
+const vuetify = createVuetify()
 
 const app = createApp(App)
 
